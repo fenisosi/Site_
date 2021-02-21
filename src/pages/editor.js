19c971d2aa035e5852ff0c7253c5da90e9f98817
fenisosi <henriquefranchesco@gmail.com>
2021-02-21 13:02:46 -0300

    Editor--

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default () => {
     const container = document.createElement('div');
 
@@ -7,24 +6,22 @@ export default () => {
         return subst;
     }
     var template = `
-    <div id="app" class="d-flex flex-column container-fluid">
-    <div id="wrapper" class="row">
-        <div class="">
+    <div id="app" >
+    <div id="wrapper">
+        <div>
         
-            <ul class="notas">
-                <li class="nav-item"><a href= "" class="na" @click.prevent="create()">Nova anotaçao</a></li>
-                <li class="nav-item" v-for="note, index in notes">
-                    <a href= "" class="nav-link" @click.prevent="select(index)">{{  note.content  }}</a>
-                </li>
-                
-            </ul>
+            <ul>
+                    <li><a href= "" class="nav-link" @click.prevent="create()">Nova anotaçao</a></li>
+                    <li v-for="note, index in notes">
+                        <a href= ""  @click.prevent="select(index)">{{  note.content  }}</a>
+                    </li>
+                    
+                </ul>
         </div>
-        <div class="col">
-            <textarea @keyup="save()" ref="textarea" placeholder="insira sua anotaçao!" class="content">{{(active !== null) ? notes[active].content  : ''}}</textarea>
-        </div>
+        <br>
     </div>
-    <div id="footer" class="bg-info">
-        <h6><strong>Dica:</strong> apague o conteudo para remover!</h6>
+    <div class="col">
+        <textarea @keyup="save()" ref="textarea" placeholder="insira sua anotaçao!" class="content">{{(active !== null) ? notes[active].content  : ''}}</textarea>
     </div>
 </div>
     `
@@ -33,39 +30,3 @@ export default () => {
     return container;
     
 }
-=======
-export default () => {
-    const container = document.createElement('div');
-
-    function capitalizeFirst(str) {
-        var subst = str.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-        return subst;
-    }
-    var template = `
-    <div id="app" class="d-flex flex-column container-fluid">
-    <div id="wrapper" class="row">
-        <div class="">
-        
-            <ul class="notas">
-                <li class="nav-item"><a href= "" class="na" @click.prevent="create()">Nova anotaçao</a></li>
-                <li class="nav-item" v-for="note, index in notes">
-                    <a href= "" class="nav-link" @click.prevent="select(index)">{{  note.content  }}</a>
-                </li>
-                
-            </ul>
-        </div>
-        <div class="col">
-            <textarea @keyup="save()" ref="textarea" placeholder="insira sua anotaçao!" class="content">{{(active !== null) ? notes[active].content  : ''}}</textarea>
-        </div>
-    </div>
-    <div id="footer" class="bg-info">
-        <h6><strong>Dica:</strong> apague o conteudo para remover!</h6>
-    </div>
-</div>
-    `
-    container.innerHTML = template;
-
-    return container;
-    
-}
->>>>>>> e91f80934eeea8da6b6f52a1a0ef031985d0055d
--- src/pages/load.js
+++ src/pages/load.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default (nome) => {
     const container = document.createElement('div');
 
@@ -35,41 +34,3 @@ export default (nome) => {
     return container;
     
 }
-=======
-export default (nome) => {
-    const container = document.createElement('div');
-
-    function capitalizeFirst(str) {
-        var subst = str.toLowerCase().replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-        return subst;
-    }
-    function typeWriter(elemento) {
-        const textoArray = elemento.innerHTML.split('');
-        elemento.innerHTML = '';
-        textoArray.forEach((letra, i) => {
-          setTimeout(() => elemento.innerHTML += letra, 75 * i);
-        });
-      }
-
-
-    const template = `
-    Olá ${capitalizeFirst(nome)}
-    <div class="geral">
-        <div class="fundo">
-          Aqui Temos Diverços Conteudos Como 2 Blogs e Aulas De Programaçao!<br>
-          Para Acessar As Aulas Vá Na Aba Aulas Ou Clique <a href="/#aulas" style="text-decoration: underline; color: red;">Aqui</a><br>
-          Para Acessar O Blog Dá Luisa Clique <a href="/#luisa" style="text-decoration: underline; color: red;">Aqui</a><br> 
-          Para Acessar O Blog Do Hennrique Clique <a href="/#henrique" style="text-decoration: underline; color: red;">Aqui</a>
-        </div>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <p>Direitos Autorais:</p>
-        <div>Ícone <img src="./imagens/Menu.png"> feito por <a href="https://www.freepik.com" title="Freepik" target="blank">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon" target="blank">www.flaticon.com</a></div>
-        <br><br><br>
-    `;
-
-    container.innerHTML = template;
-
-    return container;
-    
-}
->>>>>>> e91f80934eeea8da6b6f52a1a0ef031985d0055d
