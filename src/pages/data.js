423814abfbd948fd5153faf84450a8279bc48925
fenisosi <henriquefranchesco@gmail.com>
2021-02-28 16:54:14 -0300

    Log-in Email e Senha

--- src/pages/data.js
+++ src/pages/data.js
@@ -9,7 +9,7 @@ export default (nome, id, img, email, coocks) => {
     if (localStorage.henriques_site_method != "deslogado") {
         template = `
         <div class="data">
-        <center>${img == "Logado Com Email e Senha"?"Não Tem Imagem De Perfil":'<img src="${img}" class="img-circle">'}<br></center>
+        <center>${img == "Logado Com Email e Senha"?"Não Tem Imagem De Perfil":`<img src="${img}" class="img-circle">`}<br></center>
         <center>Olá ${capitalizeFirst(nome)}<br></center>
         <center>Temos os Seguintes Dados Sobre Você:<br></center>
         <center>Seu Id: ${id}<br></center>
