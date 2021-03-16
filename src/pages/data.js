7c21de8801b3f31fc5b41842a765f1e929d4d618
fenisosi <henriquefranchesco@gmail.com>
2021-03-16 14:34:17 -0300

    data++

--- src/pages/data.js
+++ src/pages/data.js
@@ -18,16 +18,25 @@ export default (nome, id, img, email, coocks, userId, money, rank, level, items)
             <center>Seu Email: ${email}<br></center>
             <center>Aceitou os Cookies? ${coocks}</center><br>
             <center><h2>DISCORD DATA:</h2></center>
-            <center>Seu ID No Discord: ${userId}</center>
-            <center>Suas Moedas Do Meui6: ${money}</center>
-            <center>Seu Level: ${level}</center>
-            <center>Seu Rank: ${rank}</center>
-            <center>Seus Items: \n${items.replace(/_/g, " ")}</center>
-            <br>
-            <br>
-            <center><button id="votar">Votar</button></center>
-            </div>
-            `;
+            `
+            if (userId != "Não Cadastrado No Discord") {
+                template += `
+                <center>Seu ID No Discord: ${userId}</center>
+                <center>Suas Moedas Do Meui6: ${money}</center>
+                <center>Seu Level: ${level}</center>
+                <center>Seu Rank: ${rank}</center>
+                <center><h2>Seus Items: </h2></center>
+                ${items.replace(/\*/g, "")}
+                <br>
+                <br>
+                </div>
+                `;
+            } else {
+                template += `
+                <center>Não Cadastrado No Discord!</center>
+                <center><a href="https://discord.com/api/oauth2/authorize?client_id=797559448188354571&permissions=8&scope=bot">Clique Aqui Para Adicionar o Bot!</a></center>
+                `
+            }
         } else {
             template = `
             <div class="data"> 
--- src/pages/log-in.js
+++ src/pages/log-in.js
@@ -1,7 +1,7 @@
 export default () => {
     const container = document.createElement('div');
 
-    const template = `<center><a href="/#sing-in" class="a">Sing-In</a>/Log-in</center><br><center><h6>Meu Primeiro Login Não Foi Com o Goole <input id="com_o_google" type="checkbox" name="" value=""> Meu Primeiro Login Foi Com o Goole</h6></center><br><br>
+    const template = `<center><a href="#sing-in" class="a">Sing-In</a>/Log-in</center><br><center><h6>Meu Primeiro Login Não Foi Com o Goole <input id="com_o_google" type="checkbox" name="" value=""> Meu Primeiro Login Foi Com o Goole</h6></center><br><br>
     <center><label for="e-maila" id="e-emailb">Email: </label><input type="email" id="e-maila"></center><br>
     <center><label for="senhaa" id="com_o_google2">Senha: </label><input type="password" id="senhaa"></center><br>
     <center><button id="logar">Logar</button></center>
